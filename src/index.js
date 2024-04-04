b9ac6d546d20dac7a33ef5602e294cd983b0eda5
shubhamsingh83 <akashsingh832001@gmail.com>
2024-04-04 13:15:55 +0530

    first

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import SpellCheckApp from "./SpellCheckApp";
+import SpellCheckFunc from "./SpellCheckFunc";
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+          <SpellCheckApp />
+    {/* <SpellCheckFunc /> */}
   </React.StrictMode>
 );
 
